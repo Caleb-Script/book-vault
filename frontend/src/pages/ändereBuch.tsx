b62fe6e682f13b4b8e5d7dddb1ab059b0617f002
Yasin Enes Ayyildiz <ayyildiz.yasinenes03@gmail.com>
2024-11-13 12:34:17 +0100

    design-pages

--- frontend/src/pages/ändereBuch.tsx
+++ frontend/src/pages/ändereBuch.tsx
@@ -1,4 +1,7 @@
-
-<div>
-
-</div>
+import React from 'react';
+function aendereBuch(){
+    <div>
+        <h1>Servus</h1>
+        <p>Gebe eine isbn ein, um das gewünschte Buch zu ändern.</p>
+    </div>
+}
