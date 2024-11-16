f50331597b0be5c45826a6dfb6fcf08332104b35
Yasin Enes Ayyildiz <ayyildiz.yasinenes03@gmail.com>
2024-11-16 21:45:37 +0100

    design - routes - route-buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
-import * as React from 'react';
-import { Box } from "@chakra-ui/react/box";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'next-themes';
+import Homepage from './pages/homepage';
+import DetailAnsicht from './pages/detailAnsicht';
+import ÄndereBuch from './pages/ändereBuch';
+import NeuesBuch from './pages/neuesBuch';
+import SucheBuch from './pages/sucheBuch';
 
-function App()  {
-  return(
-    <Box>
-      servus
-    </Box>
+const App: React.FC = () => {
+  return (
+    <ThemeProvider attribute="class">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/detailAnsicht" element={<DetailAnsicht />} />
+          <Route path="/ändereBuch" element={<ÄndereBuch />} />
+          <Route path="/neuesBuch" element={<NeuesBuch />} />
+          <Route path="/sucheBuch" element={<SucheBuch />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
