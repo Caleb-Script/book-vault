e6ab36dfe02ff2a72734ac58d97419329dcf219d
Caleb-Script <caleb-script@outlook.de>
2024-12-02 15:19:16 +0100

    bugfix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'next-themes';
 import Homepage from './pages/homepage';
 import DetailAnsicht from './pages/detailAnsicht';
-import ÄndereBuch from './pages/ändereBuch';
-import NeuesBuch from './pages/neuesBuch';
-import SucheBuch from './pages/sucheBuch';
+// import ÄndereBuch from './pages/ändereBuch';
+// import NeuesBuch from './pages/neuesBuch';
+// import SucheBuch from './pages/sucheBuch';
 
 const App: React.FC = () => {
   return (
@@ -14,9 +14,9 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/detailAnsicht" element={<DetailAnsicht />} />
-          <Route path="/ändereBuch" element={<ÄndereBuch />} />
+          {/* <Route path="/ändereBuch" element={<ÄndereBuch />} />
           <Route path="/neuesBuch" element={<NeuesBuch />} />
-          <Route path="/sucheBuch" element={<SucheBuch />} />
+          <Route path="/sucheBuch" element={<SucheBuch />} /> */}
         </Routes>
       </Router>
     </ThemeProvider>
