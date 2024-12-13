19b4b0c2f1bc45bf0019a33f6b66304ff90ab37d
Caleb-Script <caleb-script@outlook.de>
2024-12-13 13:06:02 +0100

    chore: detailsPage

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,20 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import client from './api/apolloClient';
 import App from './App';
+import { Box } from '@chakra-ui/react/box';
+import { ClientOnly, Skeleton } from '@chakra-ui/react';
+import { ColorModeToggle } from './components/color-mode-toggle';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <Provider>
         <Router>
+          <Box pos="absolute" top="4" right="4">
+            <ClientOnly fallback={<Skeleton w="10" h="10" rounded="md" />}>
+              <ColorModeToggle />
+            </ClientOnly>
+          </Box>
           <App />
         </Router>
       </Provider>
